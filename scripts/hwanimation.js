6411652a1ea6065c4322214105e308cc99e8d4fd
Ronak Patel <ronak1.patel@ryerson.ca>
2018-07-22 15:44:28 -0400

    Added Back to Top and some CSS
    I added a button to go back to the top of the page. I also customized the jumbotron.

---
+++ scripts/hwanimation.js
@@ -0,0 +1,9 @@
+function myTyped(){
+  var typed = new Typed('#typed', {
+  strings: ["Student", "Developer", "Technology Enthusiast"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  loop: true,
+  loopCount: Infinity
+  });
+}
