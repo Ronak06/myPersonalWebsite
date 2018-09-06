d948073ffa620ceffef284c655e223bc9aa6fe79
Ronak Patel <ronak1.patel@ryerson.ca>
2018-09-06 10:29:26 -0400

    Uploading latest code of website

---
+++ scripts/hwanimation.js
@@ -0,0 +1,10 @@
+function myTyped(){
+  var typed = new Typed('#typed', {
+  strings: ["Student", "Developer", "Technology Enthusiast"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  loop: true,
+  shuffle: false,
+  loopCount: Infinity
+  });
+}
---
+++ scripts/scrollUp.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+$('#scroll-up').click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 900);
+});
+});
