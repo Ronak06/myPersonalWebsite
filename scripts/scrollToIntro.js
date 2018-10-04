f7c633239ea6a0ec9be6e7ee2014e2b6d5157fcc
Ronak Patel <ronak1.patel@ryerson.ca>
2018-10-03 23:28:07 -0400

    Added two js scripts
    First script toggles navbar when screen size is small. Second script provides scroll animation to the Introduction section.

---
+++ scripts/scrollToIntro.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#intro').click(function() {
+        $("html, body").animate({
+            scrollTop: $('#introduction').position().top
+        }, 1000);
+    });
+});
+    
