8e0f89fc879272f6d0e0b2c57b7f56f7a5b90960
Ronak Patel <ronak1.patel@ryerson.ca>
2018-12-24 16:48:54 -0500

    Updated navigation bar and design of website

--- scripts/typedanimation.js
+++ scripts/typedanimation.js
@@ -1,6 +1,6 @@
 function myTyped(){
   var typed = new Typed('#typed', {
-  strings: ["Student", "Developer", "Technology Enthusiast"],
+  strings: ["Student.", "Developer.", "Technology Enthusiast."],
   typeSpeed: 40,
   backSpeed: 40,
   loop: true,
